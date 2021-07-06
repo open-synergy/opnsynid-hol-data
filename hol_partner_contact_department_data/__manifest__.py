91192aac7ca11b1d424d4f121e6613ccc935e5d7
Andhitia Rama <andhitia.r@gmail.com>
2021-07-06 08:04:13 +0700

    [hol_partner_contact_department_data] 11.0.1.0.0

---
+++ hol_partner_contact_department_data/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2021 OpenSynergy Indonesia
+# Copyright 2021 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "HOL Partner Contact Department Data",
+    "version": "11.0.1.0.0",
+    "category": "Partner Management",
+    "license": "AGPL-3",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "depends": [
+        "partner_contact_department",
+    ],
+    "data": [
+        "data/res_partner_department_data.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
