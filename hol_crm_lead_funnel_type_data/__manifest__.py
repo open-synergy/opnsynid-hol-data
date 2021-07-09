8458d724375e8de0ffdc5d65c7806bbdcdc32134
mikevhe18 <viriyananda.michael@gmail.com>
2021-07-09 15:00:27 +0700

    [hol_crm_lead_funnel_type_data] 11.0.1.0.0

---
+++ hol_crm_lead_funnel_type_data/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2021 OpenSynergy Indonesia
+# Copyright 2021 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "HOL CRM Lead Funnel Type Data",
+    "version": "11.0.1.0.0",
+    "category": "CRM",
+    "license": "AGPL-3",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "depends": [
+        "crm_lead_funnel_type",
+    ],
+    "data": [
+        "data/crm_lead_funnel_type_data.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
