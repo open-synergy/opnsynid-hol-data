d083d62e47339327140f0b7d7993e5dad7357149
Andhitia Rama <andhitia.r@gmail.com>
2022-04-11 20:40:52 +0700

    [hol_account_invoice_subscription_type] 11.0.1.0.0

---
+++ hol_account_invoice_subscription_type/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "HOL Subscription Type on Invoice",
+    "version": "11.0.1.0.0",
+    "license": "AGPL-3",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "depends": [
+        "subscription_payment_schedule",
+        "subscription_hierarchy",
+    ],
+    "data": [
+        "views/account_invoice_views.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
