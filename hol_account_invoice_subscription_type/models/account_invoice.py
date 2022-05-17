0aaaa464b5d4843d9b88fbc73dc5ca1dc8bdd179
Andhitia Rama <andhitia.r@gmail.com>
2022-05-18 05:10:23 +0700

    [hol_account_invoice_subscription_type] 11.0.1.1.0

--- hol_account_invoice_subscription_type/models/account_invoice.py
+++ hol_account_invoice_subscription_type/models/account_invoice.py
@@ -12,20 +12,41 @@ class AccountInvoice(models.Model):
     _name = "account.invoice"
 
     @api.depends(
-        "source_document_res_id",
-        "source_document_model_id",
+        "subscription_id",
+        "subscription_id.type",
     )
     def _compute_subscription_type(self):
         for record in self:
-            if not record.source_document_id:
-                result = "invalid"
-            else:
-                if record.source_document_model == "sale.subscription":
-                    result = record.source_document_id.type
-                else:
-                    result = "invalid"
+            result = "invalid"
+            if record.subscription_id:
+                result = record.subscription_id.type
             record.subscription_type = result
 
+    @api.depends(
+        "source_document_res_id",
+        "source_document_model_id",
+    )
+    def _compute_subscription(self):
+        for record in self:
+            result = False
+
+            if (
+                record.source_document_model == "sale.subscription"
+                and record.source_document_res_id != 0
+            ):
+                subscriptions = self.env["sale.subscription"].search(
+                    [("id", "=", record.source_document_res_id)]
+                )
+                if len(subscriptions) > 0:
+                    result = subscriptions[0]
+            record.subscription_id = result
+
+    subscription_id = fields.Many2one(
+        string="# Subscription",
+        comodel_name="sale.subscription",
+        compute="_compute_subscription",
+        store=True,
+    )
     subscription_type = fields.Selection(
         string="Subscription Type",
         selection=[
