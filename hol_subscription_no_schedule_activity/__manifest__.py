1708f3c620f5f1e4768ec166861ab665e4053f29
Andhitia Rama <andhitia.r@gmail.com>
2022-05-18 15:35:43 +0700

    [hol_subscription_no_schedule_activity] 11.0.1.0.0

---
+++ hol_subscription_no_schedule_activity/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "HOL Acvity for No Schedule",
+    "version": "11.0.1.0.0",
+    "license": "LGPL-3",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "depends": [
+        "subscription_payment_schedule",
+        "base_automation",
+    ],
+    "data": [
+        "data/ir_actions_server_data.xml",
+        "data/base_automation_data.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
