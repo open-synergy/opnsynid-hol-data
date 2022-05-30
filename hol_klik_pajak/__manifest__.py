0232367d935a983e03e46b667de1d2dafc5723b5
Andhitia Rama <andhitia.r@gmail.com>
2022-05-30 15:35:44 +0700

    [hol_klik_pajak] 11.0.1.0.1

---
+++ hol_klik_pajak/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Klik Pajak Integration",
+    "version": "11.0.1.0.0",
+    "license": "AGPL-3",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "depends": [
+        "hol_efaktur",
+        "configuration_helper",
+    ],
+    "data": [
+        "views/res_config_settings_views.xml",
+        "views/account_invoice_views.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
