54df7a4a7183c9221c6f27b127ba72adb7ab5920
Andhitia Rama <andhitia.r@gmail.com>
2022-12-28 04:57:48 +0700

    [hol_klik_pajak] 11.0.1.0.3

--- hol_klik_pajak/__manifest__.py
+++ hol_klik_pajak/__manifest__.py
@@ -3,7 +3,7 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Klik Pajak Integration",
-    "version": "11.0.1.0.2",
+    "version": "11.0.1.0.3",
     "license": "LGPL-3",
     "website": "https://simetri-sinergi.id",
     "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
